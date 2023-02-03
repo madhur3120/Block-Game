9cef2922dbf31a550b2dbd0c0225f89a565e427c
Madhur Saxena <saxenamadhur96@gmail.com>
2023-02-03 18:36:00 +0530

    game patch - 1 added obstacles for level 1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public float RestartDelay=1f;
+    bool gamehasended = false;
+    public GameObject completeLevelUI;
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+
+     public void EndGame()
+    {
+        if(gamehasended==false)
+        {
+        gamehasended=true;
+        Debug.Log("GAME OVER");
+        Invoke("Restart",RestartDelay);
+        }
+    }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+
+    public float forwardforce=1000f;
+    public float sidewaysforce=500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,forwardforce* Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sidewaysforce* Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sidewaysforce* Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if(rb.position.y<-1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
+ 
